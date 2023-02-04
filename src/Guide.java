b55606f858d80f0cb8d048e4e94d19af0f482dac
TapTap <117764947+curadprogrammer@users.noreply.github.com>
2023-02-04 15:16:21 -0800

    Add files via upload

---
+++ src/Guide.java
@@ -0,0 +1,26 @@
+public class Guide {
+    public class Main{
+        public static void main(String[] args) {
+            System.out.println("How to Guide\n" +
+                    "\n" +
+                    "Welcome :)" +
+                    "\n" +
+                    "\n" +
+                    "Use the keys or a mouse to select letters in order to guess the correct letters in the word.\n" +
+                    "\n" +
+                    "A character chosen in the word bank will change from black to gray.\n"+
+                    "\n"+
+                    "An incorrect character chosen from the word bank will change from black to red\n" +
+                    "\n" +
+                    "When the player guesses a character correctly, the character will then appear in the blank.\n" +
+                    "\n" +
+                    "When the player guesses an incorrect character, a body part will appear on the hangman post.\n" +
+                    "\n" +
+                    "Players can win the game by guessing the word before reaching the completion of the hangman's body.\n" +
+                    "\n" +
+                    "Players will lose if they collect all parts of the hangman body before guessing the word.  \n");
+        }
+    }
+
+
+}
